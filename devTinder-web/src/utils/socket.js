d51dd4d8e3cbba44df087d6f173fc09fcdd47ada
Konaparthi Surya Vamsi <159868253+K-SURYA-VAMSI@users.noreply.github.com>
2025-07-23 11:43:07 +0530

    Update socket.js

--- devTinder-web/src/utils/socket.js
+++ devTinder-web/src/utils/socket.js
@@ -2,10 +2,8 @@ import io from 'socket.io-client';
 
 
 export const createSocketConnection=()=>{
-    if(location.hostname === "localhost"){
-        return io(import.meta.env.VITE_BASE_URL || "http://localhost:1511");
-    }else{
-        return io("/",{path:"/api/socket.io"});
-    }
+   
+        return io(import.meta.env.VITE_BASE_URL);
+   
    
 }
